a6c84e5c78c3b6ada60379fca959d69dddfdd320
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-10-29 15:29:47 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,16 +9,14 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*CATBOT created by BASIL SER*
+*Creator number : wa.me/919544685703?text=Hi%20Ameer%20Suhail.%20*
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*Githublink (Setup)  :    https://github.com/itsmebasil0/catbot*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Audio commads :   https://github.com/itsmebasil0/catbot/tree/master/uploads*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Sticker commads : https://github.com/itsmebasil0/catbot/tree/master/uploads*
 
 *Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
 
