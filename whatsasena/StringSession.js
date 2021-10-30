b5a9d1b66b2a2f428ecb2e776be653a0eac211cc
Akshayser <91824378+Akshayser@users.noreply.github.com>
2021-10-30 22:00:12 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('CONNECTION_CODE' in process.env && string === undefined) {
+        if ('JULIE_MWOL in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'CATBOT;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'AchuMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
