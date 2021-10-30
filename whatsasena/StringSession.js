ec95eef4dcf4585331e3620c49b2a6ec1fcfe2e6
BASIL SIR <92247645+Itsmebasil0@users.noreply.github.com>
2021-10-30 17:14:33 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'catbot;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
